6a759f275a7fbd513f32e804facaa2f5fde2fffc
Anders Johansson <Anders Johansson>
2017-01-12 21:04:19 +0100

    comment add

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,6 +17,7 @@ class UserController extends Controller {
 
 		$memberships = Membership::with(array('role', 'team'))->where('user_id', '=', $user->id)->get();
 		return view('profile', ['memberships' => $memberships]);
+        //Johan is the best
     }
 	
 }
