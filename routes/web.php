47203de75068f2eb4e8b1f06fc9382fbdbe1764c
Anders Johansson <Anders Johansson>
2017-01-13 11:34:40 +0100

    Started on activation emails, added repo folder

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,8 @@ Route::get('/home', 'HomeController@index');
 
 //Show profile
 Route::get('/profile', 'UserController@show')->middleware('auth');;
+
+//ActivationRoute
+//TODO - It is best to put it under the Route::auth() ?
+//TODO - Change URL?
+Route::get('user/activation/{token}', 'Auth\AuthController@activateUser')->name('user.activate');
