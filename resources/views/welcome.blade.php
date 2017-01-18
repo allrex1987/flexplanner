365d1d8797700f3489bf576eb4de97a50cfe129a
Anders Johansson <Anders Johansson>
2017-01-18 22:03:48 +0100

    playing around with css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,24 +2,22 @@
 
 @section('content')
 <div class="row">
-	<div class="col-md-8 col-md-offset-2">
-		<div class="col-md-6">
+		<div class="col-md-4 col-md-offset-4 text-center">
+			{{ Html::image('/images/logo.png', 'a picture', array('class' => 'icon')) }}
 			<h1>Briefer</h1>
-			<p> Briefer är ett <b>extremt slimmat</b>, <b>enkelt</b> och <b>effektivt verktyg när du jobbar i team</b>. Bygg ditt team, skapa unika brifer, beställ material inom nätverket och planera ditt arbete på ett kick.<br>
+			<p class="buff"> Briefer är ett <b>extremt slimmat</b>, <b>enkelt</b> och <b>effektivt verktyg när du jobbar i team</b>. Bygg ditt team, skapa unika brifer, beställ material inom nätverket och planera ditt arbete på ett kick.<br>
 			<br><b>Glöm</b> dina wordmallar, gantscheman i Excel, Sharepoint och andra <b>krångliga system</b>. </p>
 		</div>
-
-		<div class="col-md-4 col-md-offset-1">
-		<br><br><br>
-			<a class="accept btn" href=""><span class="glyphicon glyphicon-hand-right"></span>Testa gratis</a>
-			<br>
-			<a href="/login" class="btn default"><span class="glyphicon glyphicon-log-in"></span> Logga in</a>
+	</div>
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			<a class="accept btn big-btn btn-center" href=""><span class="glyphicon glyphicon-hand-right"></span>Testa gratis</a>
 		</div>
 	</div>
 </div>
 
-<div class="row custom-panel">
-	<div class="col-md-8 col-md-offset-2">
+<div class="row custom-panel buff-big">
+	<div class="col-md-8 col-md-offset-2 ">
 		<div class="col-md-3">
 			{{ Html::image('/images/3.png', 'a picture') }}
 			<h2>1. Bygg ditt nätverk</h2>
